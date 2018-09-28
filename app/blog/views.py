933ccd7dfa08dde0c4f4cce2e4edb237cd5ec884
teachmesomething2580 <teachmesomething2580@gmail.com>
2018-09-27 19:25:32 -0700

    post_list view에서 template를 loader를 통해 가져와 render한 결과를 리턴

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,25 +1,33 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
 from django.utils import timezone
 import os
 
 
 def post_list(request):
-
-    # templates/blog/post_list.html 파일의 내용을 일거온 후,
-    # 해당 내용을 아래에 리턴해주는 HttpResponse인스턴스 생성 시 인수로 넣어준다.
-    #   os.path.abspath(__file__)           <- 코드가 실행중인 파일의 경로를 나타냄
-    #   os.path.dirname(<경로>)              <- 특정 경로의 상위폴더를 나타냄
-    #   os.path.join(<경로>, <폴더/파일명>)    <- 특정 경로에서 하위폴더 또는 하위 파일을 나타냄
-
-    current_path = os.path.abspath(__file__)
-    parent_app_blog_dir = os.path.dirname(current_path)
-    parent_dir = os.path.dirname(parent_app_blog_dir)
-    post_list_html = os.path.join(parent_dir, 'templates', 'blog', 'post_list.html')
-
-    content = open(post_list_html, 'rb').read()
-
+    # 템플릿을 가져옴
+    template = loader.get_template('blog/post_list.html')
+    # 해당 템플릿을 렌더링
+    context = {}
+    content = template.render(context, request)
     return HttpResponse(content)
+
+
+    # # templates/blog/post_list.html 파일의 내용을 일거온 후,
+    # # 해당 내용을 아래에 리턴해주는 HttpResponse인스턴스 생성 시 인수로 넣어준다.
+    # #   os.path.abspath(__file__)           <- 코드가 실행중인 파일의 경로를 나타냄
+    # #   os.path.dirname(<경로>)              <- 특정 경로의 상위폴더를 나타냄
+    # #   os.path.join(<경로>, <폴더/파일명>)    <- 특정 경로에서 하위폴더 또는 하위 파일을 나타냄
+    #
+    # current_path = os.path.abspath(__file__)
+    # parent_app_blog_dir = os.path.dirname(current_path)
+    # parent_dir = os.path.dirname(parent_app_blog_dir)
+    # post_list_html = os.path.join(parent_dir, 'templates', 'blog', 'post_list.html')
+    #
+    # content = open(post_list_html, 'rb').read()
+    #
+    # return HttpResponse(content)
 
     # 현재 지역에 맞는 시간&날짜 객체 할당
     # current_time = timezone.now()
